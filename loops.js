791ee251792f90071ccbbc3d35b71902c723bca6
sparsh <09wojak09@gmail.com>
2026-03-17 09:56:42 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,15 @@
+var i;
+var linebreak = "<br>";
+
+for(i=0; i<3; i=i+1){
+    console.log(i);
+}
+
+var count;
+
+console.log("Starting loop" + "<br/>");
+
+for(count = 0; count <10; count++){
+    console.log("Current Count:" + count);
+    console.log(linebreak);
+}
