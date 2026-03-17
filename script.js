5f2c93c2f3682141922f030a803b7e78f26e8b2f
sparsh <09wojak09@gmail.com>
2026-03-17 09:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("Hello World");
+console.log("Hello World");
+var linebreak = "<br>";
+console.log(linebreak);
+
+/* Null is a value 
+the difference btw undefined is not declared and null is assigned to avoid garbage value
+JavaScript is case sensitive
+javascript key words - break, export, this, byte, interface etc
+two declaration let and const, let- local, var- global....local is better bcz it is providing freedom to assign diff names in the code
+1 varible cannot be declared multiple times, it can be initialsed 
+let can be assign value later
+but const cannot reinitialised and redeclared 
+
+javascript- data value types
+1}Primitive type : single value (JS auto detect type of value that has been assigned)
+eg} number, string.boolean, undefined, null
+
+//function
+var sayHello = function(){
+    alert('Hello world!');
+}
+
+//array
+var numberArray = [1,2,3];
+
+var animals = new Array{"cat", "dog", "monkey", "lion"};
+
+//object
+
+
+
+NOTE - == is used to compare values without checking data types
+=== is used to compare value along with data types
+
+all the non zero values are always true in operators
+
+Ternary op - x=(condition)? true:false
+
+Q) FIND MAXIMUM OUT OF 3 NUMBER USING TERNARY OPERATOR using JS
+
+
+*/
