b4f516f300883deab04dc318c08f31cf59f235f5
sparsh <09wojak09@gmail.com>
2026-03-17 09:55:05 +0530

    Add files via upload

---
+++ jsoperators.js
@@ -0,0 +1,12 @@
+var a= 10;
+var b= 20;
+var linebreak ="<br>";
+
+console.log("a-b=");
+result = a-b;
+console.log(result);
+console.log(linebreak);
+
+result = (a+=b);
+console.log(result);
+console.log(linebreak);
