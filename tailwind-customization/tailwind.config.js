8e699e20c99d5b0cde41886815a4e10e90761f62
sparsh singh <iamsparsh00321@sparshs-MacBook-Air.local>
2026-05-12 16:28:31 +0530

    Add tailwind customization project

---
+++ tailwind-customization/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: "#1e3a8a",
+      },
+    },
+  },
+  plugins: [],
+}
